87ba9a4747f0bfe510db57be9d07d159b0c8ca6b
Sue Ceklosky <cekloskys@chc.edu>
2022-02-03 10:42:00 -0500

    Started Create Actors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
+# each route consists of 3 parts: 1. HTTP method, 2. URL to web page
+# view being requested, 3. name of Ruby class and method envoked when
+# URL to web page view is requested.
 Rails.application.routes.draw do
+  # When a get request is made for the actors URL,
+  # Rails envokes the index method in the actors Ruby controller class.
+  # Rails automatically maps the index method to a web page view named
+  # index.html.erb and serves the web page to the browser where its
+  # rendered.
   get "actors" => "actors#index"
+  get "actors/new" => "actors#new"
+  # When a post request is made for the actors URL,
+  # Rails envokes the create method in the actors Ruby controller class
+  post "actors" => "actors#create"
 end
